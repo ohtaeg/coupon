9ded5112dfc68d9729ae729a1655ef723b236fd9
ohtaeg <otk1090@naver.com>
2024-06-11 15:36:14 +0900

    feat (api) : 레디스 분산락, mysql record lock 로직 추가

--- coupon-negoking/coupon-api/src/main/java/fc/coupon/api/service/CouponIssueRequestService.java
+++ coupon-negoking/coupon-api/src/main/java/fc/coupon/api/service/CouponIssueRequestService.java
@@ -1,23 +1,53 @@
 package fc.coupon.api.service;
 
 import fc.coupon.api.controller.dto.CouponIssueRequestDto;
+import fc.coupon.core.component.DistributeLockExecutor;
 import fc.coupon.core.service.CouponIssueService;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class CouponIssueRequestService {
-    private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
+    private static final String LOCK_PREFIX = "lock_";
+
     private final CouponIssueService couponIssueService;
+    private final DistributeLockExecutor lockExecutor;
 
     public void issueV1(CouponIssueRequestDto requestDto) {
+        // this.issueV1WithSynchronized(requestDto);
+        this.issueV1WithRedisDistributeLock(requestDto);
+        // this.issueV1WithXLock(requestDto);
+        log.info("쿠폰 발급 완료. couponId : %s, userId : %s".formatted(requestDto.couponId(), requestDto.userId()));
+    }
+
+    /**
+     * synchronized는 어플리케이션에 종속되기 때문에
+     * 여러 서버로 확장되면 lock을 관리할 수 없어 분산락을 구현해야한다.
+     */
+    public void issueV1WithSynchronized(CouponIssueRequestDto requestDto) {
         synchronized (this) {
             couponIssueService.issue(requestDto.couponId(), requestDto.userId());
         }
-        log.info("쿠폰 발급 완료. couponId : %s, userId : %s".formatted(requestDto.couponId(), requestDto.userId()));
+    }
 
+    /**
+     * 레디스 분산락
+     */
+    public void issueV1WithRedisDistributeLock(CouponIssueRequestDto requestDto) {
+        final String lockName = LOCK_PREFIX + requestDto.couponId();
+        lockExecutor.execute(lockName, 5_000, 5_000,
+                () -> couponIssueService.issue(requestDto.couponId(), requestDto.userId())
+        );
+    }
+
+
+    /**
+     * mysql X LOCK
+     */
+    public void issueV1WithXLock(CouponIssueRequestDto requestDto) {
+        couponIssueService.issueWithXLock(requestDto.couponId(), requestDto.userId());
     }
 }
